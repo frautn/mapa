21d00d0e593a679948c944f5074d82fe1864a0d1
frautn <epalazzo@fra.utn.edu.ar>
2026-07-28 21:09:44 -0300

    Removed article from home page.

--- mdpsite/main/views.py
+++ mdpsite/main/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 from .models import Article
 
 def home(request):
-    articles = Article.objects.all().order_by('-created_at')
-    return render(request, 'main/home.html', {'articles': articles})
+    return render(request, 'main/home.html')
